b5477e58f9af335d79d79ad50d10cf7dc13b7661
Dona <Dona@Dona-PC>
2017-02-26 23:31:04 +0900

    555

--- HelloGitTest/src/HelloGit01.java
+++ HelloGitTest/src/HelloGit01.java
@@ -1,9 +1,8 @@
-
 public class HelloGit01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git 222 !!!");
+		System.out.println("Hello Git 555 !!!");
 	}
 
 }
